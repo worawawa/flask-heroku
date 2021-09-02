556995328f0319801eda98a44454058c14907a16
worawawa <89581419+worawawa@users.noreply.github.com>
2021-09-02 11:05:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,9 @@ def hello():
 def get_api():
     return jsonify(data)
 
+@app.route('/hi')
+@app.route('/hello')
+def hi():
+    return "สวัสดี"    
 if __name__ == "__main__":
     app.run(debug=False)
