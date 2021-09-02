8abbed0e9163de01fbd067af94904a478c5192cb
worawawa <89581419+worawawa@users.noreply.github.com>
2021-09-02 11:10:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,6 @@ def get_api():
 @app.route('/hi')
 @app.route('/hello')
 def hi():
-    return "สวัสดี"    
+    return "สวัสดีค้าบท่านสมาชิก"    
 if __name__ == "__main__":
     app.run(debug=False)
